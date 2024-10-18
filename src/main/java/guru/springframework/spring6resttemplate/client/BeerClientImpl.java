c578e3c7c9c64d845604c481e8dbb1e085ccd46c
David <dbcdavid.clk@gmail.com>
2024-10-18 16:16:52 -0300

    Added update, create and delete methods to beerclient

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -11,6 +11,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
+import java.util.UUID;
+
 @Service
 @RequiredArgsConstructor
 public class BeerClientImpl implements BeerClient {
@@ -18,6 +21,7 @@ public class BeerClientImpl implements BeerClient {
     private final RestTemplateBuilder restTemplateBuilder;
 
     private static final String GET_BEER_PATH = "/api/v1/beer";
+    private static final String GET_BEER_BY_ID_PATH = "/api/v1/beer/{beerId}";
 
     @Override
     public Page<BeerDTO> listBeers() {
@@ -47,5 +51,31 @@ public class BeerClientImpl implements BeerClient {
                 restTemplate.getForEntity(uriComponentsBuilder.toUriString(), BeerDTOPageImpl.class);
 
         return response.getBody();
+    }
+
+    @Override
+    public BeerDTO getBeerById(UUID beerId) {
+        RestTemplate restTemplate = restTemplateBuilder.build();
+        return restTemplate.getForObject(GET_BEER_BY_ID_PATH, BeerDTO.class, beerId);
+    }
+
+    @Override
+    public BeerDTO createBeer(BeerDTO beerDTO) {
+        RestTemplate restTemplate = restTemplateBuilder.build();
+        URI uri = restTemplate.postForLocation(GET_BEER_PATH, beerDTO);
+        return restTemplate.getForObject(uri.getPath(), BeerDTO.class);
+    }
+
+    @Override
+    public BeerDTO updateBeer(BeerDTO beerDTO) {
+        RestTemplate restTemplate = restTemplateBuilder.build();
+        restTemplate.put(GET_BEER_BY_ID_PATH, beerDTO, beerDTO.getId());
+        return getBeerById(beerDTO.getId());
+    }
+
+    @Override
+    public void deleteBeer(UUID beerId) {
+        RestTemplate restTemplate = restTemplateBuilder.build();
+        restTemplate.delete(GET_BEER_BY_ID_PATH, beerId);
     }
 }
--- src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
+++ src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
@@ -1,14 +1,87 @@
 package guru.springframework.spring6resttemplate.client;
 
+import guru.springframework.spring6resttemplate.model.BeerDTO;
+import guru.springframework.spring6resttemplate.model.BeerStyle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.web.client.HttpClientErrorException;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 class BeerClientImplTest {
 
     @Autowired
     BeerClientImpl beerClient;
+
+    @Test
+    void deleteBeer(){
+        BeerDTO newDTO = BeerDTO.builder()
+                .upc("12345")
+                .quantityOnHand(500)
+                .price(new BigDecimal(20.50))
+                .beerStyle(BeerStyle.STOUT)
+                .beerName("Mango Bobs 2")
+                .build();
+
+        BeerDTO savedDTO = beerClient.createBeer(newDTO);
+
+        assertNotNull(savedDTO);
+
+        beerClient.deleteBeer(savedDTO.getId());
+        assertThrows(HttpClientErrorException.class, () -> {
+           beerClient.getBeerById(savedDTO.getId());
+        });
+    }
+
+    @Test
+    void updateBeer(){
+        BeerDTO newDTO = BeerDTO.builder()
+                .upc("12345")
+                .quantityOnHand(500)
+                .price(new BigDecimal(20.50))
+                .beerStyle(BeerStyle.STOUT)
+                .beerName("Mango Bobs 2")
+                .build();
+
+        BeerDTO savedDTO = beerClient.createBeer(newDTO);
+
+        final String newName = "Mango Bobs 3";
+        savedDTO.setBeerName(newName);
+        BeerDTO updatedDTO = beerClient.updateBeer(savedDTO);
+
+        assertEquals(newName, updatedDTO.getBeerName());
+    }
+
+    @Test
+    void createBeer(){
+        BeerDTO newDTO = BeerDTO.builder()
+                .upc("123456")
+                .quantityOnHand(500)
+                .price(new BigDecimal(20.50))
+                .beerStyle(BeerStyle.STOUT)
+                .beerName("Mango Bobs")
+                .build();
+
+        BeerDTO savedDTO = beerClient.createBeer(newDTO);
+
+        assertNotNull(savedDTO);
+    }
+
+    @Test
+    void getBeerById(){
+        Page<BeerDTO> beerDTOs = beerClient.listBeers();
+        BeerDTO dto = beerDTOs.getContent().get(0);
+        BeerDTO byId = beerClient.getBeerById(dto.getId());
+
+        assertNotNull(byId);
+    }
 
     @Test
     void listBeersNoName() {
